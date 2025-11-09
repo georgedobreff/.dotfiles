406f820b7b3dab3c91cea5b983821e6a5e19393a
George (Witcher) Dobreff <210741271+georgedobreff@users.noreply.github.com>
2025-11-09 20:00:15 +0000

    .

--- .bashrc
+++ .bashrc
@@ -6,3 +6,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # Make an alias for invoking commands you use constantly
 # alias p='python'
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- .config/hypr/scripts/startapps.sh
+++ .config/hypr/scripts/startapps.sh
@@ -3,8 +3,6 @@
 # LAUNCH ALL APPS IN THE BACKGROUND
 omarchy-launch-webapp "https://gemini.google.com" &
 sleep 1
-omarchy-launch-editor &
-sleep 1
 alacritty --class=StartupTerminal &
 omarchy-launch-browser &
 omarchy-launch-webapp "https://github.com/" &
@@ -19,7 +17,6 @@ sleep 2
 
 # --- Workspace 1 ---
 hyprctl dispatch movetoworkspacesilent 1, class:^chrome-gemini.google.com__-Default
-hyprctl dispatch movetoworkspacesilent 1, class:^Alacritty
 hyprctl dispatch movetoworkspacesilent 1, class:^StartupTerminal
 
 # --- Workspace 2 ---
@@ -39,9 +36,6 @@ sleep 2
 hyprctl dispatch workspace 1
 hyprctl dispatch focuswindow class:^chrome-gemini.google.com__-Default
 hyprctl dispatch resizeactive -400 0
-
-hyprctl dispatch focuswindow class:^Alacritty
-hyprctl dispatch resizeactive 0 350
 
 hyprctl dispatch focuswindow class:^StartupTerminal
 hyprctl dispatch resizeactive 0 -350
